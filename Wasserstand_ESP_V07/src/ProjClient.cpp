3d5f4920b92486ba9f617e4271c859aff8cd7fef
gzehentner <130297323+gzehentner@users.noreply.github.com>
2025-10-05 09:01:24 +0200

    Zertifikat von LetsEncrypt

--- Wasserstand_ESP_V07/src/ProjClient.cpp
+++ Wasserstand_ESP_V07/src/ProjClient.cpp
@@ -42,7 +42,12 @@
 //      - im notepad++ formatieren (siehe unten)
 // 3. error.log Datei löschen und neu erstellen
 //     Fehlermeldung fd=49 "reset by peer" deutet darauf hin, dass die datei übergelaufen ist
+// !!! neu
+// ich habe jetzt ein Zertifikat von Lets encrypt über folgenden Link:
+//   https://letsencrypt.org/certs/isrgrootx1.pem
+// das soll 10 Jahre halten
 
+#ifdef blaced_root_cert
 const char *root_ca_chain =
     "-----BEGIN CERTIFICATE-----\n"
     "MIIFBTCCAu2gAwIBAgIQWgDyEtjUtIDzkkFX6imDBTANBgkqhkiG9w0BAQsFADBP\n"
@@ -74,116 +79,152 @@ const char *root_ca_chain =
     "wVD89qSTlnctLcZnIavjKsKUu1nA1iU0yYMdYepKR7lWbnwhdx3ewok=\n"
     "-----END CERTIFICATE-----\n";
 
-int errCnt_communication = 0;
+#else
+// usint the root certification from Lets encrypt - should be valid since 2035
+const char *root_ca_chain =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+    "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+    "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+    "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+    "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+    "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+    "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+    "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+    "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+    "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+    "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+    "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+    "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+    "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+    "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+    "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+    "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+    "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+    "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+    "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+    "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+    "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+    "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+    "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+    "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+    "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+    "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+    "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+    "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
+    "-----END CERTIFICATE-----\n";
+#endif
 
-void sendPost(PumpStatus &pumpStatus, PumpControl &pumpControl)
-// send data as POST to another webserver
-// V3 no Arduino String class for sending data
-{
+  int errCnt_communication = 0;
 
-  WiFiClientSecure wificlient;
-  wificlient.setCACert(root_ca_chain);
-
-  HTTPClient client;
-  const size_t MESSAGE_SIZE_MAX = 300; // maximum bytes for Message Buffer
-  char message[MESSAGE_SIZE_MAX];      // the temporary sending message - html body
-  char val[32];                        // buffer to convert floats and integers before appending
-
-  strcpy(message, "board="); // Append chars
-  strcat(message, TXT_BOARDID);
-
-  strcat(message, "&levelAct=");
-  itoa(myValueFilteredAct, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&debug_level_switches=");
-  itoa(debugLevelSwitches, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&AHH=");
-  itoa(val_AHH, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&AH=");
-  itoa(val_AH, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&AL=");
-  itoa(val_AL, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&pump1_op=");
-  itoa(pumpControl.pump1_op, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&pump2_op=");
-  itoa(pumpControl.pump2_op, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&epochTime=");
-  itoa(epochTime, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&pump1_operationTime=");
-  itoa(pumpStatus.pump1_operationTime, val, 10);
-  strcat(message, val);
-
-  strcat(message, "&pump2_operationTime=");
-  itoa(pumpStatus.pump2_operationTime, val, 10);
-  strcat(message, val);
-
-  client.begin(wificlient, sendHttpTo);                                  // Specify request destination
-  client.addHeader("Content-Type", "application/x-www-form-urlencoded"); // Specify content-type header
-  int httpCode = client.POST(message);                                   // Send the request
-  client.writeToStream(&Serial);                                         // Debug only: Output of received data
-  Serial.print(F("\nhttpCode: "));
-  Serial.println(httpCode);
-  Serial.print(F("\nmessage: ")); // Print HTTP return code;
-  Serial.println(message);
-  client.end(); // Close connection
-
-  if (simError == 1)
-  {
-    httpCode = -66;
-    simError = 0; // reset after one error
-  }
-  // error handling
-  if (httpCode < 0)
+  void sendPost(PumpStatus &pumpStatus, PumpControl &pumpControl)
+  // send data as POST to another webserver
+  // V3 no Arduino String class for sending data
   {
 
-    // write to file
-    String errMessage = "";
-    errMessage = currentDate;
-    errMessage += " - ";
-    errMessage += formattedTime;
-    errMessage += " - ";
-    errMessage += " httpCode = ";
-    errMessage += httpCode;
-    errMessage += "\n";
-    appendFile("/error.log", errMessage.c_str());
+    WiFiClientSecure wificlient;
+    wificlient.setCACert(root_ca_chain);
 
-    errCnt_communication++;
+    HTTPClient client;
+    const size_t MESSAGE_SIZE_MAX = 300; // maximum bytes for Message Buffer
+    char message[MESSAGE_SIZE_MAX];      // the temporary sending message - html body
+    char val[32];                        // buffer to convert floats and integers before appending
 
-    // ein Neustart wg fehlerhafter Kommunikation führt zu instabilem System
-    // am wichtigsten ist die Pumpensteuerung
+    strcpy(message, "board="); // Append chars
+    strcat(message, TXT_BOARDID);
 
-    // if (errCnt_communication > ERR_CNT_COMMUNICATION)
-    // {
-    //   // reset ESP8266
-    //   errMessage =  currentDate ;
-    //   errMessage += " - " ;
-    //   errMessage += formattedTime;
-    //   errMessage += " - " ;
-    //   errMessage +=  "client connection error - restart triggered\n";
-    //   appendFile("/error.log", errMessage.c_str());
-    //   ESP.restart();
-    // }
-  }
-  else
-  {
-    // reset to zero, when communication is running again
-    errCnt_communication = 0;
-  }
+    strcat(message, "&levelAct=");
+    itoa(myValueFilteredAct, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&debug_level_switches=");
+    itoa(debugLevelSwitches, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&AHH=");
+    itoa(val_AHH, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&AH=");
+    itoa(val_AH, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&AL=");
+    itoa(val_AL, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&pump1_op=");
+    itoa(pumpControl.pump1_op, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&pump2_op=");
+    itoa(pumpControl.pump2_op, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&epochTime=");
+    itoa(epochTime, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&pump1_operationTime=");
+    itoa(pumpStatus.pump1_operationTime, val, 10);
+    strcat(message, val);
+
+    strcat(message, "&pump2_operationTime=");
+    itoa(pumpStatus.pump2_operationTime, val, 10);
+    strcat(message, val);
+
+    client.begin(wificlient, sendHttpTo);                                  // Specify request destination
+    client.addHeader("Content-Type", "application/x-www-form-urlencoded"); // Specify content-type header
+    int httpCode = client.POST(message);                                   // Send the request
+    client.writeToStream(&Serial);                                         // Debug only: Output of received data
+    Serial.print(F("\nhttpCode: "));
+    Serial.println(httpCode);
+    Serial.print(F("\nmessage: ")); // Print HTTP return code;
+    Serial.println(message);
+    client.end(); // Close connection
+
+    if (simError == 1)
+    {
+      httpCode = -66;
+      simError = 0; // reset after one error
+    }
+    // error handling
+    if (httpCode < 0)
+    {
+
+      // write to file
+      String errMessage = "";
+      errMessage = currentDate;
+      errMessage += " - ";
+      errMessage += formattedTime;
+      errMessage += " - ";
+      errMessage += " httpCode = ";
+      errMessage += httpCode;
+      errMessage += "\n";
+      appendFile("/error.log", errMessage.c_str());
+
+      errCnt_communication++;
+
+      // ein Neustart wg fehlerhafter Kommunikation führt zu instabilem System
+      // am wichtigsten ist die Pumpensteuerung
+
+      // if (errCnt_communication > ERR_CNT_COMMUNICATION)
+      // {
+      //   // reset ESP8266
+      //   errMessage =  currentDate ;
+      //   errMessage += " - " ;
+      //   errMessage += formattedTime;
+      //   errMessage += " - " ;
+      //   errMessage +=  "client connection error - restart triggered\n";
+      //   appendFile("/error.log", errMessage.c_str());
+      //   ESP.restart();
+      // }
+    }
+    else
+    {
+      // reset to zero, when communication is running again
+      errCnt_communication = 0;
+    }
 }
 
 void sendWhatsAppMessage(String message)
